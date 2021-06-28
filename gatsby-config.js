b17125f33ff5d1c41410abdc60cbe1e971c362e1
sherwinski <sherwin@imgix.com>
2021-06-28 15:16:08 -0700

    config: connect app to contentful account via env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
