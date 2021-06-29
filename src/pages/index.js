6bed431bce4eeafcc464cc8bd3deb28f2644b1d3
sherwinski <sherwin@imgix.com>
2021-06-28 20:54:13 -0700

    feat: display all @imgix/contentful images via the ImgixGatsbyImage component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { ImgixGatsbyImage } from '@imgix/gatsby';
 
-export default function Home() {
-  return <div>Hello world!</div>;
+export default function Home({ data }) {
+  return(
+    <div >
+      {data.allContentfulBlogPost.edges.map(({ node }) => (
+        <div>
+          <ImgixGatsbyImage
+            // Must be an imgix URL
+            src={node.imgUrl}
+            imgixParams={{
+              auto:'format,compress',
+              crop:'faces,edges'
+            }}
+            layout="constrained"
+            width={350}
+            aspectRatio={16 / 9}
+            alt="An imgix-served image from Contentful"
+          />
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export const query = graphql`
