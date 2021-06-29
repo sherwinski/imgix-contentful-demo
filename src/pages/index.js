da817382b2105660117ea88850b406bb1d434637
sherwinski <sherwin@imgix.com>
2021-06-29 11:40:11 -0700

    feat(css): add stylings to page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,32 @@
 import React from "react";
 import { graphql } from "gatsby";
-import { ImgixGatsbyImage } from '@imgix/gatsby';
+import { ImgixGatsbyImage } from "@imgix/gatsby";
+import parse from "html-react-parser";
 
 export default function Home({ data }) {
-  return(
-    <div >
-      {data.allContentfulBlogPost.edges.map(({ node }) => (
-        <div>
-          <ImgixGatsbyImage
-            // Must be an imgix URL
-            src={node.imgUrl}
-            imgixParams={{
-              auto:'format,compress',
-              crop:'faces,edges'
-            }}
-            layout="constrained"
-            width={350}
-            aspectRatio={16 / 9}
-            alt="An imgix-served image from Contentful"
-          />
-        </div>
-      ))}
+  return (
+    <div class="p-4">
+      <div class="flex flex-wrap">
+        {data.allContentfulBlogPost.edges.map(({ node }) => (
+          <div class="p-4 lg:w-1/3 md:w-1/2 sm:w-full">
+            <p class="text-2xl p-2">{parse(node.title)}</p>
+            <ImgixGatsbyImage
+              src={node.imgUrl}
+              imgixParams={{
+                auto: "format,compress",
+                crop: "faces,edges",
+              }}
+              layout="constrained"
+              width={350}
+              aspectRatio={16 / 9}
+              alt="An imgix-served image from Contentful"
+            />
+            <p class="p-2">{parse(node.description.description)}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export const query = graphql`
