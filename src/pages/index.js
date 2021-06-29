743bc03c4e8f6c7912a68f607242aeed754a09fb
sherwinski <sherwin@imgix.com>
2021-06-28 20:53:29 -0700

    feat(graphql): define query to generate desired fields

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { graphql } from "gatsby";
 
 export default function Home() {
   return <div>Hello world!</div>;
 }
+
+export const query = graphql`
+query {
+  allContentfulBlogPost {
+    edges {
+      node {
+        imgUrl
+        title
+        description {
+          description
+        }
+      }
+    }
+  }
+}
+`
