63b70addf4d7abbe8e7f0074fc2180774c45a1fd
sherwinski <sherwin@imgix.com>
2021-06-29 23:39:48 -0700

    feat: add row of images from webfolder source
    The webfolder source points to `https://images.ctfassets.net/{space_id}`.
    The rest of the file path is extracted (albeit in a hacky way) to create a fully qualified URL, which is passed through to the imgix component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,54 @@ import { ImgixGatsbyImage } from "@imgix/gatsby";
 
 export default function Home({ data }) {
   return (
-    <div class="p-4">
-      <div class="flex flex-wrap">
-        {data.allContentfulBlogPost.edges.map(({ node }) => (
-          <div class="p-4 lg:w-1/3 md:w-1/2 sm:w-full">
-            <p class="text-2xl p-2">{parse(node.title)}</p>
-            <ImgixGatsbyImage
-              src={node.imgUrl}
-              imgixParams={{
-                auto: "format,compress",
-                crop: "faces,edges",
-              }}
-              layout="constrained"
-              width={350}
-              aspectRatio={16 / 9}
-              alt="An imgix-served image from Contentful"
-            />
-            <p class="p-2">{parse(node.description.description)}</p>
-          </div>
-        ))}
+    <div>
+      <div className="p-4">
+        <div className="flex flex-wrap">
+          {data.allContentfulBlogPost.edges.map(({ node }) => (
+            <div className="p-4 lg:w-1/3 md:w-1/2 sm:w-full">
+              <p className="text-2xl p-2">{node.title} with Webfolder</p>
+              <ImgixGatsbyImage
+                src={
+                  `https://sherwinski-contentful-wf.imgix.net/` +
+                  node.heroImage.file.url.split("/").slice(4).join("/")
+                }
+                imgixParams={{
+                  auto: "format,compress",
+                  crop: "faces,edges",
+                }}
+                layout="constrained"
+                width={350}
+                aspectRatio={16 / 9}
+                sizes="(min-width: 1024px) calc(30vw - 128px), (min-width: 768px) calc(50vw - 100px), calc(100vw - 70px)"
+                alt="An imgix-served image from Contentful"
+              />
+              <p className="p-2">{node.description.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="p-4">
+        <div className="flex flex-wrap">
+          {data.allContentfulBlogPost.edges.map(({ node }) => (
+            <div className="p-4 lg:w-1/3 md:w-1/2 sm:w-full">
+              <p className="text-2xl p-2">{node.title} with imgix-contentful</p>
+              <ImgixGatsbyImage
+                src={node.imgUrl}
+                imgixParams={{
+                  auto: "format,compress",
+                  crop: "faces,edges",
+                }}
+                layout="constrained"
+                width={350}
+                aspectRatio={16 / 9}
+                sizes="(min-width: 1024px) calc(30vw - 128px), (min-width: 768px) calc(50vw - 100px), calc(100vw - 70px)"
+                alt="An imgix-served image from Contentful"
+              />
+              <p className="p-2">{node.description.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
